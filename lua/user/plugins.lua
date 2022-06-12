24ed51b1ae41e8ef459d1d27f118755712083d5d
fe-lipe-git <felipecosta8449@hotmail.com>
2022-06-12 09:36:48 -0300

    (add) comment

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -140,6 +140,7 @@ return packer.startup(function(use)
 	use("nvim-telescope/telescope.nvim")
 	use("nvim-telescope/telescope-media-files.nvim")
 	use("nvim-telescope/telescope-file-browser.nvim")
+	-- use("ThePrimeagen/git-worktree.nvim")
 	-- Telescope --
 
 	-- Treesitter --
