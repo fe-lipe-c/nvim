82f256cb148c8ddeaff7127f989448c5f2d35e15
Felipe Costa <felipecosta8449@hotmail.com>
2022-10-19 14:22:15 -0300

    minor changes

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -57,9 +57,9 @@ cmp.setup({
 		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		["<C-c>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		["<M>"] = cmp.mapping({
+		["<M-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}), --<C-e>
@@ -69,9 +69,21 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
 			elseif luasnip.expandable() then
 				luasnip.expand()
-			elseif luasnip.expand_or_jumpable() then
+			elseif check_backspace() then
+				fallback()
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		}),
+		["<C-Space>"] = cmp.mapping(function(fallback)
+			if luasnip.expand_or_jumpable() then 
 				luasnip.expand_or_jump()
 			elseif check_backspace() then
 				fallback()
